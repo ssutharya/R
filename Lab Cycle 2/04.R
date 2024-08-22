7fa5929428cdc264ca01ea395df39f0b17e9bdc8
Sutharya <120108404+ssutharya@users.noreply.github.com>
2024-08-22 20:02:35 +0530

    update: mean and standard deviation

--- Lab Cycle 2/04.R
+++ Lab Cycle 2/04.R
@@ -5,7 +5,16 @@
 
 data(mtcars)
 
+mean_mpg <- mean(mtcars$mpg)
+sd_mpg <- sd(mtcars$mpg)
+
 hist(mtcars$mpg, breaks = 5, col = colorRampPalette(c('lightblue','blue'))(5),
      main = 'Histogram of Miles Per Gallon',
      xlab = 'Miles Per Gallon',
      ylab = 'Frequency', border = 'white')
+
+text(x = mean_mpg, y = max(hist(mtcars$mpg, plot = FALSE)$counts),
+     labels = paste('Mean =', round(mean_mpg, 2), '\nSD =', round(sd_mpg, 2)),
+     pos = 3, cex = 0.8, col = 'darkolivegreen')
+
+abline(v = mean_mpg, lwd = 2, col = 'darkkhaki')
