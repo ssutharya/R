8e466c236f73e939a20e64d8af72281b2ac5d861
Sutharya <120108404+ssutharya@users.noreply.github.com>
2024-08-23 13:13:22 +0530

    Updates

--- Lab Cycle 2/01.R
+++ Lab Cycle 2/01.R
@@ -1,12 +1,7 @@
 # scatterplot of the Sepal.Length and Petal.Length in iris dataset using the plot function.
 # proper label and title. Save the plot as a high-resolution image file.
 
-data("iris")
-
-output <- 'scatterplot_sepal_petal_length.png'
-png(filename = output, width = 800, height = 800, res = 300)
+data(iris)
 
 plot(iris$Sepal.Length, iris$Petal.Length, xlab = 'Sepal Length', ylab = 'Petal Length',
      main = 'Scatterplot of Sepal vs Petal Length', col = 'darkgreen')
-
-dev.off
